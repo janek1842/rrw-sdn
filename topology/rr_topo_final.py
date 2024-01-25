ecb3318c7cdc7e6528235d9856183bad6c58454f
Wiktor Tekiela <56120539+abcdrefg@users.noreply.github.com>
2024-01-25 11:58:11 +0100

    Added presentation and topology initializer

--- topology/rr_topo_final.py
+++ topology/rr_topo_final.py
@@ -6,21 +6,22 @@ class RrTopo(Topo):
         Topo.__init__(self)
         datacenter_dict = {}
         switch_dict = {}
-        for i in range(1, 7):
+        
+	for i in range(1,4):
             datacenter_key = 'D' + str(i)
-            switch_key = 'S' + str(i)
             datacenter_dict[datacenter_key] = self.addHost(datacenter_key)
-            switch_dict[switch_key] = self.addSwitch(switch_key)
-
+	
+	for i in range(1,3):
+	    switch_key = 'S' + str(i)
+	    switch_dict[switch_key] = self.addSwitch(switch_key)
+	
+	# Client side 
         self.addLink(switch_dict["S1"], switch_dict["S2"])
-
-        self.addLink(switch_dict["S5"], switch_dict["S1"])
-
-        self.addLink(switch_dict["S6"], switch_dict["S1"])
 	self.addLink(datacenter_dict["D1"], switch_dict["S1"])
-        # Datacenters
-        self.addLink(switch_dict["S6"], datacenter_dict["D3"])
-        self.addLink(switch_dict["S6"], datacenter_dict["D4"])
+        
+        # Server conn
+        self.addLink(switch_dict["S2"], datacenter_dict["D2"])
+        self.addLink(switch_dict["S2"], datacenter_dict["D3"])
 
 
 topos = {'rr_topo': (lambda: RrTopo())}
